a5f9972520dd3926043d91fd8157f771b5b63d14
prestonlisek32 <prestonlisek32@gmail.com>
2023-08-31 00:45:50 -0400

    created function playRound which plays a round of rock paper scissors

--- script.js
+++ script.js
@@ -14,5 +14,27 @@ function getPlayerChoice() {
 
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+function playRound(playerSelection, computerSelction) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerSelection == "paper") {
+        if(computerSelction == "scissors") {
+            return "You lose! Scissors beats paper.";
+        } else {
+            return "You win! Paper beats rock.";
+        }
+    } else {
+        if (computerSelction == "rock") {
+            return "You lose! Rock beats scissors.";
+        } else {
+            return "You win! Scissors beats paper.";
+        }
+    }
+}
+
