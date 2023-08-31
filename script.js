e8961d2c8a9d0eff590d3c1c4483a84e2141d09d
prestonlisek32 <prestonlisek32@gmail.com>
2023-08-31 00:37:17 -0400

    created function getPlayerChoice which prompts user to enter rock, paper, or scissors until they enter one of the selections, not case-sensitive

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function getComputerChoice(){
+
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
+
+function getPlayerChoice() {
+    let playerChoice;
+    while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
+        playerChoice = prompt("Please choose rock, paper, or scissors").toLowerCase();
+    }
+    return playerChoice;
+
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
