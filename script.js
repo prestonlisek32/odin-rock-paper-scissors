d908bc3d75d371cb66d0bd5a523849644a68766a
prestonlisek32 <prestonlisek32@gmail.com>
2023-09-01 01:18:43 -0400

    removed playRound and getPlayerChoice functions, as we can just pass in the player choice as a variable based on the button they press

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const ROUNDS = 5;
 let playerScore = 0;
 let computerScore = 0;
 
-playGame();
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+console.log(rock, paper, scissors);
+rock.addEventListener("click", () => {
+    alert(playRound("rock", getComputerChoice()) + `\nPlayer: ${playerScore} Computer: ${computerScore}`);
+});
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
-
-function getPlayerChoice() {
-    let playerChoice;
-
-    while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
-        playerChoice = prompt((playerChoice ? `You entered ${playerChoice}, ` : "") + "Please choose rock, paper, or scissors").toLowerCase();
-    }
-
-    return playerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -36,12 +32,6 @@ function playRound(playerSelection, computerSelection) {
         //if player didnt win, computer won
         computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-}
-
-function playGame() {
-    for (let i = 0; i < ROUNDS; i++) {
-        alert(playRound(getPlayerChoice(), getComputerChoice()) + `\nPlayer: ${playerScore} Computer: ${computerScore}`);
     }
 }
 
